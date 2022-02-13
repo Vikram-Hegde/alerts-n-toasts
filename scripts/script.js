197b4c1b5d3b323970201858bae63e1c773e99dc
Vikram-Hegde <vikramhegde1440@gmail.com>
2022-02-13 15:31:20 +0530

    insignificant

--- scripts/script.js
+++ scripts/script.js
@@ -14,6 +14,6 @@ btns.forEach((btn) =>
     Alert.alert(alertType, `this is a ${alertType} message`, {
       autoDisappear: false,
     });
-    Alert.alert(alertType, `this is a ${alertType} message`);
+    // Alert.alert(alertType, `this is a ${alertType} message`);
   })
 );
