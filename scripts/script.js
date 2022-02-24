49e1729e6c7cffa486d9a7ad810f22d7b9f86d8a
Vikram-Hegde <vikramhegde1440@gmail.com>
2022-02-24 12:50:54 +0530

    persistent theme toggle

--- scripts/script.js
+++ scripts/script.js
@@ -3,17 +3,17 @@ import Alert from './alerts.js';
 let btns = document.querySelectorAll('.add-alert');
 
 Alert.setGlobalProps({
-  timeout: 3000,
-  position: 'bottomRight',
-  autoDisappear: false,
+	timeout: 3000,
+	position: 'bottomRight',
+	autoDisappear: false,
 });
 
 btns.forEach((btn) =>
-  btn.addEventListener('click', (e) => {
-    let alertType = e.target.getAttribute('data-alert');
-    Alert.alert(alertType, `this will not disappear until manually removed`);
+	btn.addEventListener('click', (e) => {
+		let alertType = e.target.getAttribute('data-alert');
+		Alert.alert(alertType, `this will not disappear until manually removed`);
 		Alert.alert(alertType, `this alert will autoDisappear`, {
-			autoDisappear: true
+			autoDisappear: true,
 		});
-  })
+	})
 );
